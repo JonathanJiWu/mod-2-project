35423a2ce336fae7b1e917b1d160dbf6b1deac28
YOUR NAME <62856817+AllisOrder@users.noreply.github.com>
2021-09-16 11:49:46 -0400

    layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import "./App.css";
-import React from "react";
-import Img from "./img/Img";
-import SearchMovieAutoComp from "./comps/Search";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import PrimarySearchAppBar from './comps/TopBar';
 
-const App = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
   return (
-    <div>
-      {/* <Img/> */}
-      <SearchMovieAutoComp/>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+      <Grid item xs={12} sm={12}>
+          <PrimarySearchAppBar className={classes.paper}/>
+        </Grid>
+
+        <Grid item xs={12} sm={1}>
+          <Paper className={classes.paper}>space</Paper>
+        </Grid>
+        <Grid item xs={12} sm={10}>
+          <Paper className={classes.paper}>search results</Paper>
+        </Grid>
+        <Grid item xs={12} sm={1}>
+          <Paper className={classes.paper}>space</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
-};
-
-export default App;
+}
---
+++ src/comps/BackgroundImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BackgroundImg = () => {
+    return (
+        <div>
+            background
+        </div>
+    )
+}
+
+export default BackgroundImg
